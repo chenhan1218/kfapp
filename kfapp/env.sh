d5fcf67bd652838442fcd0b23011ca3293f9eb26
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2019-04-26 15:09:04 +0800

    init kfapp

---
+++ kfapp/env.sh
@@ -0,0 +1,13 @@
+PLATFORM=minikube
+KUBEFLOW_REPO=/ext/git/kubeflow
+KUBEFLOW_VERSION=master
+KUBEFLOW_COMPONENTS='"ambassador","jupyter","notebook-controller","jupyter-web-app","profiles","centraldashboard","tf-job-operator","pytorch-operator","spartakus","argo","pipeline"'
+KUBEFLOW_EXTENDEDINFO=false
+KUBEFLOW_KS_DIR=/ext/git/kubeflow/kfapp/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+DOCKER_REGISTRY_KATIB_NAMESPACE=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=minikube
+MOUNT_LOCAL=false
+DEPLOYMENT_NAME=kfapp
+
